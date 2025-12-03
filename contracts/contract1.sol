c57794b707c50b2b5820788d3e8dfb06e01c8b05
Nikita Jegorov <proxjega@gmail.com>
2025-12-03 13:55:24 +0200

    smart contract part 2

--- contracts/contract1.sol
+++ contracts/contract1.sol
@@ -7,32 +7,32 @@ contract RealEstate {
       address seller;
       address buyer;
       address agent;
-      uint32 area;
-      uint16 roomNumber;
-      string housingAdress;
+      bytes32 housingInfoHash;
       uint32 price;
       bool isActive;
       bool hasPrice;
   }
 
-  event offerCreated(Offer offer);
-  event priceOffered(uint32 price);
+  event offerCreated(uint offerNum, address assignedAgent);
+  event priceOffered(uint offerNum, uint32 price);
+  event priceAccepted(uint offerNum, uint32 price);
+  event priceDeclined(uint offerNum, uint32 price);
+  event buyerFound(uint offerNum, address buyer);
 
   mapping (uint => Offer) public offers;
   uint offerNumber = 0;
-  function createOffer(uint32 housingArea, uint16 housingRoomNumber, string memory housingAdress, address assignedAgent) public {
+  function createOffer(bytes32 hash, address assignedAgent) public {
     offerNumber+=1;
     Offer memory offer;
     offer.seller = msg.sender;
     offer.buyer = address(0);
     offer.agent = assignedAgent;
-    offer.area = housingArea;
-    offer.roomNumber = housingRoomNumber;
-    offer.housingAdress = housingAdress;
+    offer.housingInfoHash = hash;
+    offer.price = 0;
     offer.isActive = true;
     offer.hasPrice = false;
     offers[offerNumber] = offer;
-    emit offerCreated(offer);
+    emit offerCreated(offerNumber, assignedAgent);
   }
 
   function proposePrice(uint offerNum, uint32 price) public {
@@ -40,10 +40,25 @@ contract RealEstate {
     require(offers[offerNum].isActive == true);
     require(offers[offerNum].hasPrice == false);
     offers[offerNum].price = price;
-    emit priceOffered(price);
+    emit priceOffered(offerNum, price);
   }
 
   function acceptProposedPrice(uint offerNum, bool accept) public {
+    require(msg.sender == offers[offerNum].seller);
+    require(offers[offerNum].hasPrice == false);
+    require(offers[offerNum].price != 0);
+    if (accept) {
+      offers[offerNum].hasPrice = true;
+      emit priceAccepted(offerNum, offers[offerNum].price);
+    }
+    else{
+      emit priceDeclined(offerNum, offers[offerNum].price);
+    }
+  }
+
+  function findBuyer(uint offerNum, address foundBuyer) public{
     require(msg.sender == offers[offerNum].agent);
+    require(offers[offerNum].hasPrice == true);
+    offers[offerNum].buyer = foundBuyer;
   }
 }
